9d724a50410535d589e7aba8b46948f17999a83a
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-03-23 13:57:51 -0700

    added server.js

---
+++ api/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
+
+// Routers
+const usersRouter = require('../users/users-router.js');
+const authRouter = require('../auth/auth-router.js');
+const restricted = require('../auth/restricted-middleware');
+
+const server = express();
+
+// Configure Session
+const sessionConfig ={
+    name: "monkey",
+    secret: "keep it safe",
+    cookie: {
+        maxAge: 1000 * 60 * 60, //Set age to 1 Hour
+        secure: false,  //True in production to send over http
+        httpOnly: true, //Restrict js acess
+    },
+    resave: false,
+    saveUninitialized: true,
+};
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
+
+server.use("/api/users", restricted, usersRouter);
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+    res.send(<h1>Users Database</h1>);
+  });
+
+  module.exports = server;
+
+
+
